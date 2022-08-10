8aecdeb927ff742b857357bc5e9144b8ed92793a
Yujean Cho <eugenie8@kakao.com>
2022-08-10 15:08:22 +0900

    [UPDATE] Add TableView

---
+++ Outstagram/Scenes/FeedTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedTableViewCell.swift
+//  Outstagram
+//
+//  Created by Yujean Cho on 2022/08/10.
+//
+
+import SnapKit
+import UIKit
+
+final class FeedTableViewCell: UITableViewCell {
+    
+}
--- Outstagram/Scenes/FeedViewController.swift
+++ Outstagram/Scenes/FeedViewController.swift
@@ -9,11 +9,35 @@ import SnapKit
 import UIKit
 
 class FeedViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        
+        tableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "FeedTableViewCell")
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
+        setupTableView()
+    }
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell
+        cell?.selectionStyle = .none
+        
+        return cell ?? UITableViewCell()
     }
 }
 
@@ -29,6 +53,11 @@ private extension FeedViewController {
         )
         
         navigationItem.rightBarButtonItem = uploadButton
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{ $0.edges.equalToSuperview() }
     }
 }
 
