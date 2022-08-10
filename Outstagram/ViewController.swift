18168dbee075a1be0ed9b7ffbca0d1994557a0f9
Yujean Cho <eugenie8@kakao.com>
2022-08-10 13:26:20 +0900

    [CREATE] Outstagram App

---
+++ Outstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outstagram
+//
+//  Created by Yujean Cho on 2022/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
